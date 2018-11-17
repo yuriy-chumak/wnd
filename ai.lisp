9fb5b0c174779be1490cfd4d05d0dba98141c7de
Yuriy Chumak <yuriy.chumak@comodo.com>
2018-11-18 01:29:12 +0200

    first workable version

---
+++ ai.lisp
@@ -0,0 +1,94 @@
+#!/usr/bin/ol
+
+(define (A* level from-x from-y to-x to-y)
+   (let ((xy (cons from-x from-y))
+
+         (hash (lambda (xy)
+            (+ (<< (car xy) 16) (cdr xy))))
+         ; пуста ли клетка карты "в голове" персонажа, работает для любых координат, даже отрицательных
+         (floor? (lambda (x y)
+            (let by-y ((y y) (map level))
+               (if (< y 0) #true
+               (if (null? map) #true
+               (if (= y 0)
+                  (let by-x ((x x) (map (car map)))
+                     (if (< x 0) #true
+                     (if (null? map) #true
+                     (if (= x 0)
+                        (eq? (car map) 0)
+                     (by-x (- x 1) (cdr map))))))
+               (by-y (- y 1) (cdr map)))))))))
+
+   (if (and (= from-x to-x) (= from-y to-y)) ; уже пришли
+      (tuple 0 0 #empty #empty)
+   (let step1 ((n 999); количество шагов поиска
+               (c-list-set #empty)
+               (o-list-set (put #empty (hash xy)  (tuple xy #f  0 0 0))))
+      (if (eq? o-list-set #empty)
+         (tuple 0 0 #empty #empty) ; некуда идти - постоим
+
+      ; найдем клетку с минимальной стоимостью:
+      (let*((f (ff-fold (lambda (s key value)
+                           (if (< (ref value 5) (car s))
+                              (cons (ref value 5) value)
+                              s))
+                  (cons 9999 #f) o-list-set))
+;                  (_ (print "next: " f))
+            (xy (ref (cdr f) 1)) ; положение клетки с минимальным весом '(x.y)
+            ; перенесем ее из открытого в закрытый список
+            (o-list-set (del o-list-set (hash xy)))
+            (c-list-set (put c-list-set (hash xy) (cdr f))))
+
+         ;
+         (if (or (eq? n 0)
+               (and
+                  (eq? (car xy) to-x)
+                  (eq? (cdr xy) to-y)))
+            (let rev ((xy xy))
+               ; обратный проход по найденному пути, вернуть только первый шаг
+               ;  (в сторону предполагаемого маршрута
+               (let*((parent (ref (get c-list-set (hash xy) #f) 2)) ; todo: переделать
+                     (parent-of-parent (ref (get c-list-set (hash parent) #f) 2)))
+                  (if parent-of-parent (rev parent)
+                     (tuple
+                        (- (car xy) (car parent))
+                        (- (cdr xy) (cdr parent))
+                        c-list-set
+                        o-list-set
+                        ))))
+
+            ; 5: Проверяем все соседние клетки.
+            ;  Игнорируем те, которые находятся в закрытом списке или непроходимы
+            ;  (поверхность со стенами, водой), остальные добавляем в открытый список,
+            ;  если они там еще не находятся. Делаем выбранную клетку "родительской"
+            ;  для всех этих клеток.
+            (let*((x (car xy))
+                  (y (cdr xy))
+                  (o-list-set (fold (lambda (n v)
+                                 (if (and
+                                       (floor? (car v) (cdr v)) ; если туда можно передвинуться...
+                                       (eq? #f (get c-list-set (hash v) #f)))
+                                    (let ((G (+ (ref (get c-list-set (hash xy) #f) 3) 1)); G родителя + 1
+                                          ; H calculated by "Manhattan method"
+                                          ; http://www2.in.tu-clausthal.de/~zach/teaching/info_literatur/A_Star/A_star_tutorial/heuristics.htm.html
+                                          (H (* (+ (abs (- (car v) to-x))
+                                                (abs (- (cdr v) to-y))) 2))
+                                          ; 6: Если соседняя клетка уже находится в открытом списке
+                                          (got (get o-list-set (hash v) #f)))
+
+                                       ; если эта клетка уже в списке
+                                       (if got
+                                          (if (< G (ref got 3)) ; но наш путь короче
+                                             (put n (hash v)  (tuple v xy  G H (+ G H)))
+                                             ;else ничего не делаем
+                                             n)
+                                          ; else
+                                          (put n (hash v)  (tuple v xy  G H (+ G H)))))
+                                    n))
+                                 o-list-set (list
+                                                (cons x (- y 1))
+                                                (cons x (+ y 1))
+                                                (cons (- x 1) y)
+                                                (cons (+ x 1) y)))))
+               (step1 (- n 1) c-list-set o-list-set)))))))))
+
---
+++ creature.lisp
@@ -0,0 +1,107 @@
+; /home/uri/Workspace/ira/flare-game/art_src/animation_defs/characters
+
+(import (file ini))
+(import (scheme misc))
+
+; servers with numbers - creatures!
+;; (fork-server 'creatures (lambda ()
+;; (let this ((id 1) (itself #empty))
+;; (let*((envelope (wait-mail))
+;;       (sender msg envelope))
+;;    (tuple-case msg
+;;       ; low level interaction interface
+;;       ((new)
+;;          (mail sender id)
+;;          (this (+ id 1) (put itself id #true)))
+;;       ((kill id)
+;;          (this itself id (del itself id)))
+;;       ((debug)
+;;          (mail sender itself)
+;;          (this id itself)))))))
+
+
+
+(define orientations (list->ff `(
+   (0 . ,(* 3 32)) ; top
+   (1 . ,(* 4 32)) ; top-right
+   (2 . ,(* 5 32)) ; right
+   (3 . ,(* 6 32)) ; right-bottom
+   (4 . ,(* 7 32)) ; bottom
+   (5 . ,(* 0 32)) ; left-bottom
+   (6 . ,(* 1 32)) ; left
+   (7 . ,(* 2 32))))) ;left-top
+(define speed 64) ; 1 tile per second
+
+; todo: make automatic id generation
+(define (make-creature key)
+   (fork-server key (lambda ()
+   (let this ((itself #empty))
+   (let*((envelope (wait-mail))
+         (sender msg envelope))
+      (tuple-case msg
+         ; low level interaction interface
+         ((set key value)
+            (this (put itself key value)))
+         ((get key)
+            (mail sender (get itself key #false))
+            (this itself))
+         ((debug)
+            (mail sender itself)
+            (this itself))
+
+         ; set animation
+         ((set-animations ini first-gid)
+            (let*((itself (put itself 'fg first-gid))
+                  (itself (put itself 'animations ini)))
+               (this itself)))
+         ((set-current-animation animation)
+            ; calculate current animation
+            (let*((itself (put itself 'animation animation))
+                  (ssms (let* ((ss ms (clock))) (+ (* ss 1000) ms)))
+                  (itself (put itself 'ssms ssms)))
+               (this itself)))
+         ((set-location xy)
+            (let*((itself (put itself 'location xy)))
+               (this itself)))
+         ((set-orientation orientation)
+            (let*((itself (put itself 'orientation orientation)))
+               (this itself)))
+
+         ((move-relative xy)
+            (let*((location (get itself 'location '(0 . 0)))
+                  (itself (put itself 'location (cons (+ (car location) (car xy)) (+ (cdr location) (cdr xy)))))
+                  (orientation (cond
+                     ((equal? xy '(-1 . 0)) 6)
+                     ((equal? xy '(0 . -1)) 0)
+                     ((equal? xy '(+1 . 0)) 2)
+                     ((equal? xy '(0 . +1)) 4)
+                     (else (get itself 'orientation 0))))
+                  (itself (put itself 'orientation orientation)))
+               (this itself)))
+
+         ((get-location)
+            (mail sender (get itself 'location '(0 . 0)))
+            (this itself))
+         ((get-animation-frame)
+            (let*((animation (get itself 'animation 'stance)) ; соответствующая состояния анимация
+                  (ssms (- (let* ((ss ms (clock))) (+ (* ss 1000) ms)) ; количество ms с момента перехода в анимацию
+                           (get itself 'ssms 0)))
+                  (animations (get itself 'animations #empty))
+                  (animation (get animations animation #empty))
+                  (duration (get animation 'duration "250ms"))
+                  (duration (substring duration 0 (- (string-length duration) 2)))
+                  (duration (string->number duration 10))
+                  (frames (get animation 'frames "4"))
+                  (frames (string->number frames 10))
+                  (position (get animation 'position "4"))
+                  (position (string->number position 10))
+                  (orientation (get itself 'orientation 0))
+                  (frame (floor (/ (* ssms frames) duration))))
+               (mail sender (+ (get itself 'fg 0) position (mod frame frames) (get orientations orientation 0))))
+            (this itself))
+         ; 
+
+         (else
+            (print-to stderr "Unknown command " msg)
+            (this itself))))))))
+
