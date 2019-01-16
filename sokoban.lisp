c204baa5e905f724a3703a590e2017a733d06a5e
Yuriy Chumak <yuriy.chumak@mail.com>
2019-01-16 15:22:26 +0200

    научил зомби идти в указанную точку, а не только по одной клетке

--- sokoban.lisp
+++ sokoban.lisp
@@ -7,8 +7,8 @@
 (begin
    (define config (list->ff `(
       ; напомню, что мы используем фиксированный шрифт размера 9*16
-      (width  . ,(* 2  9 80))      ; 80 знакомест в ширину
-      (height . ,(* 2 16 25))))))) ; 25 знакомест в высоту
+      (width  . ,(* 1  9 80))      ; 80 знакомест в ширину
+      (height . ,(* 1 16 25))))))) ; 25 знакомест в высоту
 (import (lib gl config))
 
 ; игра пошаговая! посему все ходы только после клика "я готов" (пока это ПКМ) и все НПС
@@ -63,16 +63,13 @@
 
 ; =============================
 ; 1. Загрузим игровой уровень
-(level:load "007")
+(level:load (or (lref *vm-args* 1) "007"))
 
 ; временная функция работы с level-collision
 (define collision-data (level:get-layer 'collision))
 
 (define H (length collision-data))       ; высота уровня
 (define W (length (car collision-data))) ; ширина уровня
-
-(print "H: " H)
-(print "W: " W)
 
 ; временная функция: возвращает collision data
 ;  по координатам x,y на карте
@@ -100,6 +97,105 @@
 
 (print "grid-id: " grid-id)
 (print "gem-id: " gem-id)
+
+; =================================================================
+; A*
+; упрощенный A* алгоритм под нашу задачу.
+; допущения:
+; 1. выход за границу уровня невозможен (поэтому мы уберем проверки границ)
+;(define collision-data (level:get-layer 'collision))
+;#|
+(define (A* level from to)
+   (let*((xy from) ; начальное значение поиска пути
+         ; для быстрого обращения к элементам карты сконвертируем ее из списка в кортеж
+         ; функция (list->tuple) реализована на стороне виртуальной машины, посему очень быстрая
+         (level (list->tuple (map list->tuple level)))
+         ; получить значение из карты по координатам '(x.y), координаты начинаются с 0
+         (level-at (lambda (xy)
+            (ref (ref level (+ (cdr xy) 1)) (+ (car xy) 1))))
+         ; функция хеширования пары '(x.y), для быстрого поиска в словаре
+         (hash (lambda (xy)
+            (+ (<< (car xy) 16) (cdr xy))))
+         ; пуста ли клетка карты "в голове" персонажа, работает для любых координат, даже отрицательных
+         (floor? (lambda (xy)
+            (and
+               (eq? (level-at xy) 0)
+               (let ((o (get-object (car xy) (cdr xy))))
+                  (not (has? (list gem-id gemH-id) o)))))))
+
+   (if (equal? from to) ; а никуда идти и не надо?
+      #false ;(tuple 0 0 #empty #empty)
+   ; ищем без ограничений, это упростит алгоритм
+   (let step1 ((c-list-set #empty)
+               (o-list-set (put #empty (hash xy)  (tuple xy #f  0 0 0))))
+      (if (eq? o-list-set #empty) ; некуда идти :(
+         #false ;(tuple 0 0 #empty #empty)
+
+      ; найдем клетку с минимальной стоимостью:
+      (let*((f (ff-fold (lambda (s key value)
+                           (if (< (ref value 5) (car s))
+                              (cons (ref value 5) value)
+                              s))
+                  (cons 9999 #f) o-list-set))
+;                  (_ (print "next: " f))
+            (xy (ref (cdr f) 1)) ; положение клетки с минимальным весом '(x.y)
+            ; перенесем ее из открытого в закрытый список
+            (o-list-set (del o-list-set (hash xy)))
+            (c-list-set (put c-list-set (hash xy) (cdr f))))
+
+         ; если мы наконец добрались до нужной точки
+         (if (equal? xy to)
+            (let rev ((xy xy))
+               ; обратный проход по найденному пути, вернуть только первый шаг
+               ;  (в сторону предполагаемого маршрута
+               (let*((parent (ref (get c-list-set (hash xy) #f) 2)) ; todo: переделать
+                     (parent-of-parent (ref (get c-list-set (hash parent) #f) 2)))
+                  (if parent-of-parent (rev parent)
+                     (cons ;(tuple
+                        (- (car xy) (car parent))
+                        (- (cdr xy) (cdr parent))
+                        ;c-list-set
+                        ;o-list-set
+                        ))))
+            ; хм, пока еще не добрались?
+            ; тогда:
+            ; Проверяем все соседние клетки.
+            ;  игнорируем те, которые находятся в закрытом списке или непроходимы
+            ;  (поверхность со стенами, водой), остальные добавляем в открытый список,
+            ;  если они там еще не находятся. Делаем выбранную клетку "родительской"
+            ;  для всех этих клеток.
+            (let*((x (car xy))
+                  (y (cdr xy))
+                  (o-list-set (fold (lambda (o v)
+                                 (if (and
+                                       (floor? v)                       ; если в эту клетку можно встать
+                                       (eq? #f (get c-list-set (hash v) #f))) ; и она не в закрытом списке
+
+                                    (let ((G (+ (ref (get c-list-set (hash xy) #f) 3) 1)); G родителя + 1
+                                          ; H calculated by "Manhattan method"
+                                          ; https://web.archive.org/web/20160930004511/http://www2.in.tu-clausthal.de/~zach/teaching/info_literatur/A_Star/A_star_tutorial/heuristics.htm.html
+                                          (H (* (+ (abs (- (car v) (car to)))
+                                                   (abs (- (cdr v) (cdr to)))) 2))
+                                          ; 6: Если соседняя клетка уже находится в открытом списке
+                                          (got (get o-list-set (hash v) #f)))
+
+                                       ; если эта клетка уже в списке
+                                       (if got
+                                          (if (< G (ref got 3)) ; но наш путь короче
+                                             (put o (hash v)  (tuple v xy  G H (+ G H)))
+                                             ;else ничего не делаем
+                                             o)
+                                          ; else
+                                          (put o (hash v)  (tuple v xy  G H (+ G H)))))
+                                    o))
+                                 o-list-set (list
+                                                (cons x (- y 1))
+                                                (cons x (+ y 1))
+                                                (cons (- x 1) y)
+                                                (cons (+ x 1) y)))))
+               (step1 c-list-set o-list-set)))))))))
+;|#
+
 ; =================================================================
 ; -=( hero )=---------
 (define hero (make-creature 'hero #empty))
@@ -175,18 +271,19 @@
       (cons (floor x) (floor y))))))))
 
 ; служебная функция: вычисляет, может ли герой ходить в указываемую клетку
-(define (move-available? to)
-   (let*((herotile (creature:get-location 'hero))
-         (dx (- (car to) (car herotile)))
-         (dy (- (cdr to) (cdr herotile)))
-         (move (cons dx dy))
-         (move-available (and
-            (or
+;(define mouse-position '(-1 . -1)) ; положение, где сейчас мышка
+; один шаг, можно толкать чаши
+(define (step-available? from to)
+   (let*((dx (- (car to) (car from)))
+         (dy (- (cdr to) (cdr from)))
+         (step (cons dx dy))
+         (step-available (and
+            (or ; один шаг?
                (and (eq? dx 0) (eq? dy +1))
                (and (eq? dx 0) (eq? dy -1))
                (and (eq? dx +1) (eq? dy 0))
                (and (eq? dx -1) (eq? dy 0)))
-            (and
+            (and ; и ничего не мешает либо пройти либо толкнуть
                (eq? (at (car to) (cdr to)) 0)
                (let ((object (get-object (car to) (cdr to))))
                   (if (or (eq? object gem-id) (eq? object gemH-id))
@@ -195,7 +292,13 @@
                         (let ((o (get-object (+ (car to) dx) (+ (cdr to) dy))))
                            (and (not (eq? o gem-id)) (not (eq? o gemH-id)))))
                      #true))))))
-      move-available))
+      step-available))
+
+; можно пройти (не толкая ничего, просто пройти)
+(define (move-available? from to)
+   (and
+      (not (has? (list gem-id gemH-id) (get-object (car to) (cdr to))))
+      (A* collision-data from to)))
 
 ; init
 (glShadeModel GL_SMOOTH)
@@ -265,11 +368,14 @@
    (render-windows)
 
    ; let's draw mouse pointer
+   (define from (creature:get-location 'hero))
    (if (and mouse
             (not (unbox *calculating*)))
       (let*((mousetile (xy:screen->tile mouse))
-            (move-available (move-available? mousetile))
-            (id (+ (level:get-gid 'pointer) (if move-available 0 1)))
+            (action-available (or
+               (step-available? from mousetile)
+               (move-available? from mousetile)))
+            (id (+ (level:get-gid 'pointer) (if action-available 0 1)))
             (tile (getf (interact 'level (tuple 'get 'tileset)) id))
             (w (/ (- (ref window 3) (ref window 1)) 48)) ;  размер курсора
             (st (ref tile 5))
@@ -331,15 +437,14 @@
          (sender msg envelope))
       (tuple-case msg
          ((move to)
-            (if (move-available? to)
-               (let*((hero (creature:get-location 'hero))
-                     (rel (cons
-                        (- (car to) (car hero))
-                        (- (cdr to) (cdr hero)))))
-                  (print "object-to: " (get-object (car to) (cdr to)))
-                  (if (or
-                        (eq? (get-object (car to) (cdr to)) gem-id)
-                        (eq? (get-object (car to) (cdr to)) gemH-id))
+            (define from (creature:get-location 'hero))
+            ; сюда мы попадем либо если можно пойти на соседнее поле (возможно, с толчком чаши), либо вообще пойти куда-то
+            (if (step-available? from to)
+               (let ((rel (cons
+                        (- (car to) (car from))
+                        (- (cdr to) (cdr from)))))
+;                     (print "object-to: " (get-object (car to) (cdr to)))
+                  (if (has? (list gem-id gemH-id) (get-object (car to) (cdr to)))
                      (begin
                         (set-object (+ (car to) (car rel)) (+ (cdr to) (cdr rel))
                            (let ((background (level:get-layer 'background)))
@@ -359,9 +464,27 @@
                         (creature:set-orientation 'hero 6)))
 
                   ; и пошлем его в дорогу
-                  (creature:move-with-animation 'hero rel 'run #f)))
-            ; todo: хорошо бы добавить проверку на проигрыш...
+                  (creature:move-with-animation 'hero rel 'run #f))
+               ; иначе идем куда сказали
+               (let loop ((from from))
+                  (let ((rel (A* collision-data from to)))
+                     (if rel ; если еще не пришли
+                        ; повернем героя в нужную сторону
+                        (begin
+                           (cond
+                              ((equal? rel '(0 . -1))
+                                 (creature:set-orientation 'hero 0))
+                              ((equal? rel '(+1 . 0))
+                                 (creature:set-orientation 'hero 2))
+                              ((equal? rel '(0 . +1))
+                                 (creature:set-orientation 'hero 4))
+                              ((equal? rel '(-1 . 0))
+                                 (creature:set-orientation 'hero 6)))
+                           (creature:move-with-animation 'hero rel 'run #f)
+                           (loop (creature:get-location 'hero)))))))
+
             ; а теперь проверка на выигрыш:
+            ; todo: хорошо бы добавить еще и проверку на проигрыш...
             (let ((win? (fold (lambda (f b o)
                                  (fold (lambda (f b o)
                                           (and f (if (eq? b grid-id)
@@ -369,7 +492,11 @@
                                     f b o))
                            #true (level:get-layer 'background) (level:get-layer 'object))))
                (if win?
-                  (creature:play-animation 'hero 'cast 'cast)
+                  (begin
+                     (for-each (lambda ()
+                           (creature:play-animation 'hero 'cast 'cast))
+                        (iota 8))
+                     (print "SOMETHING!!"))
                   (set-world-busy #false)))
             (this itself))
          (else
